fc819690e98bc25d96c725709c2e94aea59cf256
Vlad Lotockij <vlad.lotockij25@gmail.com>
2022-02-21 21:33:46 +0300

    another change

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const date = require('./date')
 function foo() {
         const dt = date.bar('hh:mm DD/MM/YYYY');
         //date.bar
-        console.log('jnjdfbdf');
+        console.log('jnjdfbgfgfggfgdf');
         console.log(dt, date.b);
 }
 
