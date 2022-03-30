b22b8a7f4815567ce8f6da7fc20cba62d2bc2995
Vlad Lotockij <vlad.lotockij25@gmail.com>
2022-03-30 19:24:38 +0300

    change132

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 function foo() {
         //const dt = date.bar('hh:mm DD/MM/YYYY');
         //date.bar
-        console.log('main123');
+        console.log('main1234');
         console.log(dt, date.b);
 }
 
